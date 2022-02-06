e718e64bbe0cbf2c4e26f77af49701b64dfafcf9
quinnkhuc <71853148+quinnkhuc@users.noreply.github.com>
2022-02-06 17:45:40 -0500

    Add recent order page

--- Happy_Sunshine/profile.php
+++ Happy_Sunshine/profile.php
@@ -8,7 +8,7 @@ include_once __DIR__ . "/config.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Happy Sunshine</title>
+    <title>Profile | Happy Sunshine</title>
     <link rel="shortcut icon" type="image/svg" href="./img/favicon.svg">
     <link rel="stylesheet" href="./css/index.css">
     <link rel="stylesheet" href="./css/form.css">
