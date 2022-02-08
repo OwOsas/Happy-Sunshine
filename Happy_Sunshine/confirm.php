8b46c3b11a888c64b030c9d33429fd65dd961adb
Tran (Cheryl) Vo <71985185+tv356@users.noreply.github.com>
2022-02-08 21:22:29 +0700

    Update confirm.php

--- Happy_Sunshine/confirm.php
+++ Happy_Sunshine/confirm.php
@@ -11,8 +11,7 @@ include_once __DIR__ . "/config.php";
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Confirm Order | Happy Sunshine</title>
         <link rel="shortcut icon" type="image/svg" href="./img/favicon.svg">
-        <link rel="stylesheet" href="./css/index.css">
-        <link rel="stylesheet" href="./order.css">
+        <link rel="stylesheet" href="./css/confirm.css">
     </head>
 
     <body>
